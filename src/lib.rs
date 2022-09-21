2646b5fea3c8916601e5311066269a1ea0431266
USSURATONCACHI <redchindaniil@gmail.com>
2022-09-22 01:14:40 +0700

    Moved tests to a separate file

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
-}
-
-pub fn how() -> {
-    1i32 + 3u32
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
