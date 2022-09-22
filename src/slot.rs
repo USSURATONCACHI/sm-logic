b9a47d029f0160552e4ef124d85ec54eae31bdca
USSURATONCACHI <redchindaniil@gmail.com>
2022-09-22 18:32:55 +0700

    Changed Slot base architecture (still no implementation)

--- src/slot.rs
+++ src/slot.rs
@@ -3,6 +3,28 @@ use crate::util::Map3D;
 use crate::util::Point;
 
 #[derive(Debug, Clone)]
+pub struct BaseSlotData {
+	name: String,
+	kind: String,
+	size: Bounds,
+}
+
+#[derive(Debug, Clone)]
+pub enum SlotError {
+	NameIsAlreadyTaken {
+		main_slot_name: String,
+		subject_name: String
+	},
+
+	SlotIsOutOfBounds {
+		main_slot_name: String,
+		subject_name: String,
+		subject_size: Bounds,
+		subject_pos: Point,
+	}
+}
+
+#[derive(Debug, Clone)]
 pub struct Slot {
 	name: String,
 	kind: String,
@@ -39,11 +61,11 @@ impl Slot {
 		todo!()
 	}
 
-	pub fn add_sub_slot(&mut self, path: String, pos: Point, slot: Slot) -> Result<(), ()> {
+	pub fn add_sub_slot(&mut self, path: String, pos: Point, slot: Slot) -> Result<(), SlotError> {
 		todo!()
 	}
 
-	pub fn set_sub_slot(&mut self, path: String, pos: Point, slot: Slot) -> Result<(), ()> {
+	pub fn set_sub_slot(&mut self, path: String, pos: Point, slot: BaseSlotData) -> Result<(), SlotError> {
 		todo!()
 	}
 }
